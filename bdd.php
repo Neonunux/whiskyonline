135ed68a3fd5750b4db1a94d626599d9e0f6d201
Neonunux <regis.leloup@colombbus.org>
2013-08-12 19:55:53 +0200

    New system of connexion management

--- bdd.php
+++ bdd.php
@@ -8,6 +8,26 @@
 	// {
 		// die('Erreur : '.$e->getMessage());
 	// }
+		switch ($_SERVER['SERVER_NAME']) 
+	{
+	  case "whiskyonline.free.fr":
+		$db_host = 'sql.free.fr';
+		$db_name = 'whiskyonline';
+		$db_username = 'whiskyonline';
+		$db_password = 'MDPSQLINTERFACE3';
+	  break;
+	  case "localhost":
+		$db_host = 'localhost';
+		$db_name = 'whiskyonline';
+		$db_username = 'root';
+		$db_password = '';
+		break;
+	  default:
+		$db_host = NULL;
+		$db_name = NULL;
+		$db_username = NULL;
+		$db_password = NULL;
+	}
 	
 	$host = 'localhost';
 	$user = 'root';
